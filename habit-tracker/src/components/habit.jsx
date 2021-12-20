87f48db2154ce90b20ba2148ae6163432c6af2be
EUNKYUNG JUNG <thxjek@gmail.com>
2021-12-21 08:58:41 +0900

    Update habit function

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 
-class Habit extends Component {
-    //첫번째 컴포넌트에 생명력 불어넣어주자!( 동적으로 만들자! )
-    state = {
-        count: 0,
-    };
+//하나의 습관을 보여줄 수 있는 가장 작은 단위의 컴포넌트를 만들어 놓았다.
+//부모 컴포넌트로부터 props로 props로 전달받은 '습관의 데이터를 보여주기'만 하는 컴포넌트이기 때문에
 
+class Habit extends Component {
     // 멤버변수
     handleIncrement = () => {
         //handleIncrement는 state오브젝트 안에 있는 count를증가 한뒤 state를 업데이트 한다.
@@ -14,11 +12,15 @@ class Habit extends Component {
         //그래서 꼭! state를 update할때는 리엑트에서 제공하는 이 setState라는 함수를 호출해줘야한다.
         //따라서 부분적으로 데이터를 업데이트 할 수 없고,
         //이렇게 전체적인 state를 업데이트 해줘야 '리엑트가 state가 변경이 되었구나! render함수를 다시 호출하자! 라고 알 수 있다.'
-        this.setState({ count: this.state.count + 1 });
+        // console.log(this.props);
+        this.props.onIncrement(this.props.habit);
     };
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({ count: count < 0 ? 0 : count });
+        this.props.onDecrement(this.props.habit);
+    };
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
     };
     render() {
         //외부로부터 받아온 데이터는 props라는 클래스안에 있는 데이터에 저장되어진다.
@@ -41,7 +43,10 @@ class Habit extends Component {
                 >
                     <i className="fas fa-minus-square"></i>
                 </button>
-                <button className="habit-button habit-delete">
+                <button
+                    className="habit-button habit-delete"
+                    onClick={this.handleDelete}
+                >
                     <i className="fas fa-trash"></i>
                 </button>
             </li>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -9,13 +9,37 @@ class Habits extends Component {
             { id: 3, name: "Coding", count: 0 },
         ],
     };
+    // 멤버변수
+    handleIncrement = (habit) => {
+        // console.log(habit);
+        console.log(`handleIncrement ${habit.name}`);
+    };
+
+    handleDecrement = (habit) => {
+        console.log(`handleDecrement ${habit.name}`);
+    };
+
+    handleDelete = (habit) => {
+        console.log(`handleDelete ${habit.name}`);
+    };
     render() {
         return (
             <ul>
                 {
                     //JavaScript code {}
                     this.state.habits.map((habit) => (
-                        <Habit key={habit.id} habitArray={habit} />
+                        //각각의 습관들을 Habit컴포넌트로 전달을한다.
+                        <Habit
+                            key={habit.id}
+                            habitArray={habit}
+                            //콜백함수로 전달한다.-> handleIncrement변수는 함수를 가르키고있다.
+                            //함수는 Object의 일종이다. 그래서 handleIncrement라는 변수는 오른쪽 함수를 가르키기 있기 때문에
+                            //아래와같이 함수의 이름을 인자로 전달해주면 함수를 가르키고있는 참조값을 전달 하는 것이므로 함수를 바로
+                            //호출하는 것이 아니라 함수를 가르키고있는 참조값만 onIncrement라는 prop으로 전달해주게 된다.
+                            onIncrement={this.handleIncrement}
+                            onDecrement={this.handleDecrement}
+                            onDelete={this.handleDelete}
+                        />
                     ))
                 }
             </ul>
