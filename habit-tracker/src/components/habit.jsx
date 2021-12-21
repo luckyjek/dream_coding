50c58f1462ca904e3ded4e466eb9734cd6113766
EUNKYUNG JUNG <thxjek@gmail.com>
2021-12-21 14:43:15 +0900

    Upgrade habit

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -6,13 +6,6 @@ import React, { Component } from "react";
 class Habit extends Component {
     // 멤버변수
     handleIncrement = () => {
-        //handleIncrement는 state오브젝트 안에 있는 count를증가 한뒤 state를 업데이트 한다.
-        // this.state.count += 1; <-그냥 이렇게 오브젝트 안에있는 데이터를 하게되면 리액트는
-        //업체이트가 됐는지 안됐는지 모른다.
-        //그래서 꼭! state를 update할때는 리엑트에서 제공하는 이 setState라는 함수를 호출해줘야한다.
-        //따라서 부분적으로 데이터를 업데이트 할 수 없고,
-        //이렇게 전체적인 state를 업데이트 해줘야 '리엑트가 state가 변경이 되었구나! render함수를 다시 호출하자! 라고 알 수 있다.'
-        // console.log(this.props);
         this.props.onIncrement(this.props.habit);
     };
     handleDecrement = () => {
@@ -26,7 +19,7 @@ class Habit extends Component {
         //외부로부터 받아온 데이터는 props라는 클래스안에 있는 데이터에 저장되어진다.
         // console.log(this.props.habitArray);
         //const habitName = this.props.habitArray
-        const { name, count } = this.props.habitArray;
+        const { name, count } = this.props.habit;
         return (
             <li className="habit">
                 <span className="habit-name">{name}</span>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -12,15 +12,25 @@ class Habits extends Component {
     // 멤버변수
     handleIncrement = (habit) => {
         // console.log(habit);
-        console.log(`handleIncrement ${habit.name}`);
+        // console.log(`handleIncrement ${habit.name}`);
+        //...연산자-> https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({ habits: habits }); //value에서 쓰인것은 로컬에있는 habits!
     };
 
     handleDecrement = (habit) => {
-        console.log(`handleDecrement ${habit.name}`);
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({ habits: habits });
     };
 
     handleDelete = (habit) => {
-        console.log(`handleDelete ${habit.name}`);
+        const habits = this.state.habits.filter((item) => item.id !== habit.id);
+        this.setState({ habits });
     };
     render() {
         return (
@@ -31,7 +41,7 @@ class Habits extends Component {
                         //각각의 습관들을 Habit컴포넌트로 전달을한다.
                         <Habit
                             key={habit.id}
-                            habitArray={habit}
+                            habit={habit}
                             //콜백함수로 전달한다.-> handleIncrement변수는 함수를 가르키고있다.
                             //함수는 Object의 일종이다. 그래서 handleIncrement라는 변수는 오른쪽 함수를 가르키기 있기 때문에
                             //아래와같이 함수의 이름을 인자로 전달해주면 함수를 가르키고있는 참조값을 전달 하는 것이므로 함수를 바로
