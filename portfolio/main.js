84fa494d87a5bcdfdc02e324ee361d65151a22e3
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-02-10 21:26:33 +0900

    Update menu-bar-undeline

--- portfolio/main.js
+++ portfolio/main.js
@@ -48,6 +48,7 @@ document.getElementById('next').addEventListener('click', nextSlide);
 
 
 //Make navbar transparent when it is on the top
+
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -55,6 +56,7 @@ document.addEventListener("scroll", () => {
     // console.log(`navbarHeight:${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add("navbar--dark");
+
     } else {
         navbar.classList.remove("navbar--dark");
     }
