8490767f35ef99f3c479069981aac255730b195b
luckyjek <70943889+luckyjek@users.noreply.github.com>
2022-02-13 19:36:59 +0900

    Add coordinates

---
+++ coordinates/main.js
@@ -0,0 +1,20 @@
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(`${x} ${y}`);
+
+    /*style을 지정할 때는 px, 픽셀이라는 유닛을 붙여야된다.*/
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.innerHTML = `${x},${y}px`;
+})
